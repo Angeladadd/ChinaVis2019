f370c0a2d9b2db9b06658b840cb575e4d92923e2
angeladaddy <sunchenge1997@sjtu.edu.cn>
2019-06-09 04:43:01 +0800

    地图可以查看一段时间了，就是实现的比较粗略

--- static/scripts/DrawRoute.js
+++ static/scripts/DrawRoute.js
@@ -1,5 +1,5 @@
 sensor_people_log = new Array(0);
-function drawMainActive(day_index){
+function drawMainActive(day_index, initial_time){
     //处理参数
     var day = 0;
     switch(day_index) {
@@ -63,69 +63,7 @@ function drawMainActive(day_index){
             }
             var label_color = {'attendee':'#000000','manager':'#FF0000','reporter':'#00FF00','guest':'#999900','ordinary worker':'#990099'};
 
-            svg.selectAll(".sensor")
-                .data(sensor)
-                .enter()
-                .append("rect")
-                .attr("class","sensor")
-                .attr("id", function (d,i) {
-                    sensor_people[i] = 0;
-                    return "sensor"+i.toString();
-                })
-                .attr("y",function (d) {
 
-                    return xLinear(d.x)-sensor_width/2;
-                })
-                .attr("x",function (d) {
-                    var base = 0;
-                    if(d.floor != "1")
-                        base += bias;
-                    return base+yLinear(d.y)-sensor_width/2;
-                })
-                .attr("width",sensor_width)
-                .attr("height",sensor_width)
-                .attr("fill", function (d,i) {
-                    return color1[color_label[d.label]];
-                })
-                .style("opacity",0.5)
-                .on("mouseover",function (d,i) {
-                                Tooltip
-                                    .style("opacity", 1)
-                                    .text(sensor_people_log[i].toString())
-                                    .attr("x", (d3.mouse(this)[0]+20) + "px")
-                                    .attr("y", (d3.mouse(this)[1]) + "px");
-                                var p=document.getElementById("list");
-                                p.innerHTML = sensor_people_log[i].toString();
-                            })
-                .on("mouseout",function (d,i) {
-                                Tooltip.style("opacity",0);
-                            });
-            svg.selectAll(".position")
-                .data(people[day])
-                .enter()
-                .append("circle")
-                .attr("class","position")
-                .attr("cy", function (d,i) {
-
-                        return xLinear(d[0].x);})
-                .attr("cx", function (d,i) {
-                    var base = 0;
-                        if(d[0].floor != "1")
-                            base+=bias;
-                    return base + yLinear(d[0].y);})
-                .attr("r",2)
-                .style("fill", function (d,i) {
-                    var id = d[0].id;
-                    var label = id_label[id];
-                    return label_color[label];
-                })
-                .style("opacity",function (d,i) {
-                    if(label_choose == 'all') return 1;
-                    var id = d[0].id;
-                    var label = id_label[id];
-                    if(label == label_choose) return 1;
-                    else return 0;
-                });
 
             // function path(d){
             //     var path = d3.path();
@@ -169,38 +107,118 @@ function drawMainActive(day_index){
 
             for(var i=0;i<people[day].length;i++){
                 obj.point[i] = 0;
+                while(people[day][i][obj.point[i]].time<initial_time && obj.point[i]+1<people[day][i].length && people[day][i][obj.point[i]+1].time<initial_time){
+                        obj.point[i] +=1;
+                }
+                //console.log(obj.point[i]);
             }
+            for(var i=0;i<sensor.length;i++){
+                sensor_people[i] = 0;
+            }
+            for(var i=0;i<people[day].length;i++){
+                if(obj.point[i]==0) continue;
+                var sid = people[day][i][obj.point[i]].sensor_simple_id;
+                sensor_people[sid]+=1;
+                sensor_people_log[sid].push(people[day][i][0].id);
+            }
+
+
+            svg.selectAll(".sensor")
+                .data(sensor)
+                .enter()
+                .append("rect")
+                .attr("class","sensor")
+                .attr("id", function (d,i) {
+                    return "sensor"+i.toString();
+                })
+                .attr("y",function (d) {
+
+                    return xLinear(d.x)-sensor_width/2;
+                })
+                .attr("x",function (d) {
+                    var base = 0;
+                    if(d.floor != "1")
+                        base += bias;
+                    return base+yLinear(d.y)-sensor_width/2;
+                })
+                .attr("width",sensor_width)
+                .attr("height",sensor_width)
+                .attr("fill", function (d,i) {
+                    return compute[color_label[sensor[i].label]](color_linear(sensor_people[i]));
+                })
+                .style("opacity",0.5)
+                .on("mouseover",function (d,i) {
+                                Tooltip
+                                    .style("opacity", 1)
+                                    .text(sensor_people_log[i].toString())
+                                    .attr("x", (d3.mouse(this)[0]+20) + "px")
+                                    .attr("y", (d3.mouse(this)[1]) + "px");
+                                var p=document.getElementById("list");
+                                p.innerHTML = sensor_people_log[i].toString();
+                            })
+                .on("mouseout",function (d,i) {
+                                Tooltip.style("opacity",0);
+                            });
+            svg.selectAll(".position")
+                .data(people[day])
+                .enter()
+                .append("circle")
+                .attr("class","position")
+                .attr("cy", function (d,i) {
+                        return xLinear(d[0].x);})
+                .attr("cx", function (d,i) {
+                    var base = 0;
+                        if(d[0].floor != "1")
+                            base+=bias;
+                    return base + yLinear(d[0].y);})
+                .attr("r",2)
+                .style("fill", function (d,i) {
+                    var id = d[0].id;
+                    var label = id_label[id];
+                    return label_color[label];
+                })
+                .style("opacity",function (d,i) {
+                    if(label_choose == 'all') return 1;
+                    var id = d[0].id;
+                    var label = id_label[id];
+                    if(label == label_choose) return 1;
+                    else return 0;
+                });
+
+             var Tooltip = svg.append("text")
+            .style("opacity", 0)
+            .style("font-size",14)
+            .attr("class", "tooltip");
+            // for(var i=0;i<sensor_people.length;i++){
+            //     console.log(sensor_people[i]);
+            // }
             obj.update = function (currentTime){
+                //if(currentTime!=initial_time) currentTime=initial_time;
                 var Tooltip = obj.Tooltip;
                 for(var i=0;i<this.people.length;i++){
                     if(!this.people[i][this.point[i]+1]) continue;
                     if(this.people[i][this.point[i]+1].time == currentTime) {
-                        var old_sensor = this.people[i][this.point[i]].sensor_simple_id;
+                        console.log(i.toString());
+
                         var new_sensor = this.people[i][this.point[i]+1].sensor_simple_id;
-                        if(this.point[i]!= 0) {sensor_people[old_sensor] -= 1;
-                        sensor_people_log[old_sensor].splice(sensor_people_log[old_sensor].indexOf(this.people[i][0].id),1);
+                        console.log(new_sensor.toString());
+                        if(this.point[i]!= 0)
+                        {
+                            var old_sensor = this.people[i][this.point[i]].sensor_simple_id;
+                            sensor_people[old_sensor] -= 1;
+                            sensor_people_log[old_sensor].splice(sensor_people_log[old_sensor].indexOf(this.people[i][0].id),1);
+                            d3.select("#sensor"+old_sensor.toString())
+                            .attr("fill", compute[color_label[sensor[old_sensor].label]](color_linear(sensor_people[old_sensor])));
                         }
                         sensor_people[new_sensor] += 1;
-
                         sensor_people_log[new_sensor].push(this.people[i][0].id);
                         //console.log(this.sensor_people_log[new_sensor].toString());
-                        d3.select("#sensor"+old_sensor.toString())
-                            .attr("fill", compute[color_label[sensor[old_sensor].label]](color_linear(sensor_people[old_sensor])));
                         d3.select("#sensor"+new_sensor.toString())
                             .attr("fill", compute[color_label[sensor[new_sensor].label]](color_linear(sensor_people[new_sensor])));
-                            // .on("mouseover",function (d,i) {
-                            //     Tooltip
-                            //         .style("opacity", 1)
-                            //         .text(sensor_people_log[i].toString())
-                            //         .attr("x", (d3.mouse(this)[0] + 20) + "px")
-                            //         .attr("y", (d3.mouse(this)[1]) + "px");
-                            // })
-                            // .on("mouseout",function (d,i) {
-                            //     Tooltip.style("opacity",0);
-                            // });
                         this.point[i]+=1;
                     }
                 }
+                //console.log("666");
                 var point = this.point;
                 this.svg.selectAll(".position")
                     .transition()
