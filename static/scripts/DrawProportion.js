49550562d40d54f4fc42b10a7ddbff017567b533
angeladaddy <sunchenge1997@sjtu.edu.cn>
2019-06-10 02:14:21 +0800

    new ver

--- static/scripts/DrawProportion.js
+++ static/scripts/DrawProportion.js
@@ -1,238 +1,193 @@
-var dom = document.getElementById("proportion");
-var myChart = echarts.init(dom);
-var idx = 1;
-option = null;
-option = {
-    timeline : {
-        data : [
-            '2013-01-01', '2013-02-01', '2013-03-01', '2013-04-01', '2013-05-01',
-            { name:'2013-06-01', symbol:'emptyStar6', symbolSize:8 },
-            '2013-07-01', '2013-08-01', '2013-09-01', '2013-10-01', '2013-11-01',
-            { name:'2013-12-01', symbol:'star6', symbolSize:8 }
-        ],
-        label : {
-            formatter : function(s) {
-                return s.slice(0, 7);
-            }
-        }
-    },
-    options : [
-        {
-            title : {
-                text: '浏览器占比变化',
-                subtext: '纯属虚构'
-            },
-            tooltip : {
-                trigger: 'item',
-                formatter: "{a} <br/>{b} : {c} ({d}%)"
-            },
-            legend: {
-                data:['Chrome','Firefox','Safari','IE9+','IE8-']
-            },
-            toolbox: {
-                show : true,
-                feature : {
-                    mark : {show: true},
-                    dataView : {show: true, readOnly: false},
-                    magicType : {
-                        show: true,
-                        type: ['pie', 'funnel'],
-                        option: {
-                            funnel: {
-                                x: '25%',
-                                width: '50%',
-                                funnelAlign: 'left',
-                                max: 1700
-                            }
-                        }
-                    },
-                    restore : {show: true},
-                    saveAsImage : {show: true}
-                }
-            },
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    center: ['50%', '45%'],
-                    radius: '50%',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        },
-        {
-            series : [
-                {
-                    name:'浏览器（数据纯属虚构）',
-                    type:'pie',
-                    data:[
-                        {value: idx * 128 + 80,  name:'Chrome'},
-                        {value: idx * 64  + 160,  name:'Firefox'},
-                        {value: idx * 32  + 320,  name:'Safari'},
-                        {value: idx * 16  + 640,  name:'IE9+'},
-                        {value: idx++ * 8  + 1280, name:'IE8-'}
-                    ]
-                }
-            ]
-        }
-    ]
-};
-if (option && typeof option === "object") {
-    myChart.setOption(option, true);
+function DrawProportion() {
+    var obj = {};
+    obj.flag = false;
+    var margin = {top: 15, right: 5, bottom: 5, left: 5},
+        width = 250 - margin.left - margin.right,
+        height = 250 - margin.top - margin.bottom;
+    var svg = d3.select("#proportion")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+    var classes = ['scholar','waiter','assistant','visitor','attendee','reporter','business','cook'];
+    var day_number = ['All','1&2','1&3','2&3','1','2','3'];
+
+    // var region_people = new Array(11);
+    // for (var i = 0; i < 11; i++) {
+    //     region_people[i] = {};
+    //     region_people[i].region = region[i];
+    //     region_people[i].people = 1;
+    // }
+    var radius = Math.min(width-10, height-10) / 2,
+    g = svg.append("g")//.attr("transform", "translate(" + (width / 2 ).toString() + "," + (height/2+10) + ")");
+    var pie = d3.pie()
+        .sort(null)
+        .value(function(d) { return d.people; });
+    var color = d3.scaleOrdinal()
+        .domain(day_number)
+        .range(["#FDD8D3",
+            "#FF6F5F",
+        "#FF9D75",
+        "#FFE39F",
+        "#A7D2D6",
+        "#FFF9E9",
+        "#FF9E92"]);
+
+    // d3.select("#idx_proportion")
+    //     .attr("transform", "translate(0,5)")
+    //     .attr("width",20)
+    //     .attr("height",200)
+    //     .selectAll("rect")
+    //     .data(day_number)
+    //     .enter()
+    //     .append("rect")
+    //     .attr("y",function (d,i) {
+    //         return i*15+10;
+    //     })
+    //     .attr("width",10)
+    //     .attr("height",10)
+    //     .attr("fill",function (d) {
+    //         return color(d);
+    //     });
+    // d3.select("#idx_proportion")
+    //     .selectAll("text")
+    //     .data(day_number)
+    //     .enter()
+    //     .append("text")
+    //     .attr("y",function (d,i) {
+    //         return i*15+20;
+    //     })
+    //     .attr("x",15)
+    //     .text(function (d) {
+    //         return d;
+    //     })
+    //     .attr('fill','white')
+    //     .style("font-size",12);
+
+
+    var path = d3.arc()
+        .outerRadius(radius*0.8)
+        .innerRadius(radius*0.5);
+    //
+    var path0 = d3.arc()
+        .outerRadius(radius*0.9)
+        .innerRadius(radius*0.5);
+    var label0 = d3.arc()
+        .outerRadius(radius*0.9 + 6)
+        .innerRadius(radius*0.9 + 6);
+    //
+    var arc = g.selectAll(".arc")
+        .data(pie(people_proportion['visitor']))
+        .enter().append("g")
+        .attr("class", "arc");
+    //
+    //
+        arc.append("path")
+            .attr("d", path)
+            .attr("fill", function (d) {
+                return color(d.data);
+            });
+    //
+        arc.append("text")
+            .attr("transform", function (d) {
+                var x2 = label0.centroid(d)[0];
+                var y2 = label0.centroid(d)[1];
+                var x3 = x2 < 0 ? x2 - 10: x2 + 5;
+                return "translate(" + x3 + "," + y2 + ")";
+            })
+            .style("font-size","12px")
+            .style("opacity",0)
+            .text(function (d) {
+                return d.data;
+            });
+    //     obj.arc = arc;
+    //     obj.region_people=region_people;
+    //     obj.update=function(){
+    //         var flag = this.flag;
+    //         var local_current_people = current_people;
+    //         if(current_people_attr.region != null){
+    //             var tmp = current_people_attr.region;
+    //             current_people_attr.region = null;
+    //             local_current_people = calculate_people();
+    //             current_people_attr.region = tmp;
+    //         }
+    //         //if(people_list.length==0) return;
+    //         var obj_arc = this.arc;
+    //         var new_region_people = this.region_people;
+    //         for(var i=0;i<11;i++) new_region_people[i].people=0;
+    //         for(var i=0;i<local_current_people.length;i++){
+    //             for(var j=0;j<11;j++){
+    //                 if(local_current_people[i].region == new_region_people[j].region){
+    //                     new_region_people[j].people++;
+    //                 }
+    //             }
+    //         }
+    //         obj_arc.data(pie(new_region_people));
+    //         obj_arc.select("path")
+    //         .attr("d", path)
+    //         .attr("fill", function (d) {
+    //             return color(d.data.region);
+    //         });
+    //         obj_arc.select("text")
+    //             .attr("transform", function (d) {
+    //             var x2 = label0.centroid(d)[0];
+    //             var y2 = label0.centroid(d)[1];
+    //             var x3 = x2 < 0 ? x2 - 10 : x2 + 5;
+    //             return "translate(" + x3 + "," + y2 + ")";
+    //         })
+    //             .text(function (d) {
+    //                 if(d.data.region == current_people_attr.region){
+    //                     d3.select(this).select("path")
+    //                         .attr("d", path0);
+    //                     d3.select(this).select("text")
+    //                         .style("opacity",1);
+    //                     flag = true;
+    //                 }
+    //             return d.data.region;
+    //             });
+    //         obj_arc.on("mouseover",function(d,i) {
+    //             if(flag == true) return;
+    //     d3.select(this).select("path")
+    //         .attr("d", path0);
+    //     d3.select(this).select("text")
+    //         .style("opacity",1);
+    //     current_people_attr.region = d.data.region;
+    //     current_people = calculate_people();
+    //     if(current_people_attr.province == null) obj_province.update();
+    //     if(current_people_attr.age == null) obj_age.update();
+    //     if(current_people_attr.background == null) obj_bg.update();
+    //     if(current_people_attr.school == null) obj_school.update();
+    // });
+    //
+    //     obj_arc.on("mouseout", function (d, i) {
+    //         if(flag == true) return;
+    //         d3.select(this).select("path")
+    //             .attr("d", path);
+    //         d3.select(this).select("text")
+    //             .style("opacity",0);
+    //         current_people_attr.region = null;
+    //         current_people = calculate_people();
+    //         if(current_people_attr.province == null) obj_province.update();
+    //     if(current_people_attr.age == null) obj_age.update();
+    //     if(current_people_attr.background == null) obj_bg.update();
+    //     if(current_people_attr.school == null) obj_school.update();
+    //     })
+    //         .on("click",function (d) {
+    //             if(flag == true){
+    //                 flag = false;
+    //                 current_people_attr.region = null;
+    //                 current_people = calculate_people();
+    //                 obj_province.update();
+    //                 obj_age.update();
+    //                 obj_bg.update();
+    //                 obj_school.update();
+    //                 obj_people.update();
+    //             }
+    //             else {
+    //                 flag = true;
+    //                 obj_people.update();
+    //             }
+    //         });
+    //
+    //
+    //     };
+    //     return obj;
 }
--- static/scripts/DrawRoute.js
+++ static/scripts/DrawRoute.js
@@ -65,32 +65,36 @@ function drawMainActive(day_index, initial_time){
             var label_color = {'scholar':'#ff0000','waiter':"#00ff00",'assistant':"#ff00ff",'reporter':'#0000ff','attendee':'#000000','visitor':'#ffff00','business':'#00ffff','cook':'#33ff66'};
 
 
-            // function path(d){
-            //     var path = d3.path();
-            //     var x = yLinear(d[1].y);
-            //     var y = xLinear(d[1].x);
-            //     if(d[1].floor == '2')
-            //         x+=bias;
-            //     path.moveTo(x,y);
-            //     for(var i=2;i<d.length-1;i++){
-            //         var x = yLinear(d[i].y);
-            //         var y = xLinear(d[i].x);
-            //         if(d[i].floor == '2')
-            //             x+=bias;
-            //         path.lineTo(x,y);
-            //     }
-            //     return path;
-            // }
-            // svg.selectAll(".path")
-            // .data(people[day])
-            //     .enter()
-            //     .append("path")
-            //     .attr("class","path")
-            //     .attr("fill","none")
-            //     .attr("stroke","lightgray")
-            //     .attr("stroke-width",.5)
-            //     .attr("opacity",1)
-            //     .attr("d",path);
+            function path(d){
+                var path = d3.path();
+                var x = yLinear(d[1].y);
+                var y = xLinear(d[1].x);
+                if(d[1].floor == '2')
+                    x+=bias;
+                path.moveTo(x,y);
+                for(var i=2;i<d.length-1;i++){
+                    var x = yLinear(d[i].y);
+                    var y = xLinear(d[i].x);
+                    if(d[i].floor == '2')
+                        x+=bias;
+                    path.lineTo(x,y);
+                }
+                return path;
+            }
+            svg.selectAll(".path")
+            .data(people[day])
+                .enter()
+                .append("path")
+                .attr("class","path")
+                .attr("fill","none")
+                .attr("stroke","black")
+                .attr("stroke-width",.5)
+                .attr("opacity",1)
+                .attr("d",function (d,i) {
+                    if(day_obj.map[d[0].id]=='visitor')
+                        return path(d);
+                    else return null;
+                });
 
     var old = svg.select('text'); if(old) old.remove();
     var Tooltip = svg.append("text")
