b065e1d33ca10987f7e80fab419233b2ddeb7a19
liuyuxiang512 <liuyuxiang512@gmail.com>
2019-06-10 22:40:14 +0800

    改变条形图能根据时间变化

--- static/scripts/DrawBar1.js
+++ static/scripts/DrawBar1.js
@@ -1,18 +1,8 @@
-// console.log(D1permin);
-groups = [];
-groupData = [];
-time = [];
-for(var i in Object.keys(D1permin)){
-    groups.push(Object.keys(D1permin)[i]);
-    groupData.push(D1permin[Object.keys(D1permin)[i]]);
-    
-}
-for(var j=0; j<groupData[0].length; j++){
-    time.push(j);
-}
-// console.log(time);
-// console.log(groups);
-// console.log(groupData);
+var groups = groups1;
+var groupData = groupData1;
+var time = time1;
+
+console.log("1");
 
 var dom = document.getElementById("containerlyx11");
 var myChart = echarts.init(dom, 'dark');
@@ -258,4 +248,5 @@ option = {
 };
 if (option && typeof option === "object") {
     myChart.setOption(option, true);
-}
+};
+console.log("1");
